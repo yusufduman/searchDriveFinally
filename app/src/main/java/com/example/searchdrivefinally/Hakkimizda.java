6149a3fde937e99877d92be953bc562327a86b5e
Yusuf Duman <yduman@st.medipol.edu.tr>
2020-03-05 02:45:23 +0300

    Uygulamanın başlangıç kodlamasında yapılan ayarlar ve bazı hatalar giderilerek işleme başlanmıştır. Uygulama başlangıç ekranı olarak Splash Screen 3 milisaniye ayarlanmış olup yönlendirmesi yapılmıştır. Splash Screen yönlendirmesi kayıt ekranından alınarak login ekranına verilmiştir. Başlangıçta kodlanan "Giriş" sayfasında tasarımsal bir takım değişikliklere gidilerek sayfa tamamlanmıştır. Veri tabanı bağlantısı hali hazırda bulunduğundan Uygulama içerisinde sorunsuz bir şekilde giriş yapılabilmektedir. Kullanıcı girişinin ardından kullanıcıyı karşılayan "Anasayfa" UI ve UX açıdan tamamlanarak bitirilmiştir. Anasayfa'da bulunan Kullanıcı paneli tasarımsal açıdan tamamlanmıştır, Aynı zamanda  "Anasayfa" üzerinde bulunan "Yeni Döküman", "Döküman Kontrol", "Tasarım Ekleme", "Döküman Kontrol" butonlarının tasarımsal ve yazılımsal gereklilikleri tamamlanarak proje kaydedilmiştir.

--- app/src/main/java/com/example/searchdrivefinally/Hakkimizda.java
+++ app/src/main/java/com/example/searchdrivefinally/Hakkimizda.java
@@ -2,13 +2,60 @@ package com.example.searchdrivefinally;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Hakkimizda extends AppCompatActivity {
+    Button b1,b2,b3,b4,b5;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hakkimizda);
+        b1 = (Button)findViewById(R.id.yenidokuman);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gecisYap = new Intent(Hakkimizda.this, MainActivity.class);
+                startActivity(gecisYap);
+            }
+        });
+        b2 = (Button)findViewById(R.id.dokumankontrol);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gecisYap = new Intent(Hakkimizda.this, MainActivity.class);
+                startActivity(gecisYap);
+            }
+        });
+        b3 = (Button)findViewById(R.id.cizimekle);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gecisYap = new Intent(Hakkimizda.this, MainActivity.class);
+                startActivity(gecisYap);
+            }
+        });
+        b4 = (Button)findViewById(R.id.dokumannerede);
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gecisYap = new Intent(Hakkimizda.this, MainActivity.class);
+                startActivity(gecisYap);
+            }
+        });
+        b5 = (Button)findViewById(R.id.kullanicipaneli);
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gecisYap = new Intent(Hakkimizda.this, usercp.class);
+                startActivity(gecisYap);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/searchdrivefinally/activity_splash.java
@@ -0,0 +1,34 @@
+package com.example.searchdrivefinally;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class activity_splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(activity_splash.this, Login.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
