f1de6d5fc3eaa59d6b3dfc419f059bc415057f89
Yusuf Duman <yduman@st.medipol.edu.tr>
2020-04-02 05:22:37 +0300

    Döküman Ekleme sayfasının tasarımı ve veritabanı üzerinde kayıt oluşturma işlemlerinin ardından uygulama üzerinde veritabanı içerisinde oluşturulan dökümanları sorgulamak için kullanılan "Döküman Kontrol" sayfasının tasarımı ve veritabanı bağlantıları tamamlandı. Gereksinim Analizinde belirtilmiş olduğu gibi sayfa içerisinde var olan dökümanın numarası ile sorgulama yapabilmek amaçlanmıştır. Döküman Kontrol sayfası içerisinde döküman numarasının girileceği alana gerekli bilgi girildikten sonra sorgula butonuna basıldığında sayfa içerisinde bulunan "Listview" içerisinde tüm bilgileri yazdırmaktadır.

---
+++ app/src/main/java/com/example/searchdrivefinally/DokumanAdapter.java
@@ -0,0 +1,56 @@
+package com.example.searchdrivefinally;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
+
+public class DokumanAdapter extends BaseAdapter {
+    private Activity activity;
+    private List<Map<String, String>> dokumanListesi;
+
+    public DokumanAdapter(Activity activity, List<Map<String, String>> dokumanListesi) {
+        this.activity = activity;
+        this.dokumanListesi = dokumanListesi;
+    }
+
+    @Override
+    public int getCount() {
+        return dokumanListesi.size();
+    }
+
+    @Override
+    public Map<String, String> getItem(int position) {
+        return dokumanListesi.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = activity.getLayoutInflater().inflate(R.layout.dokuman_item, parent, false);
+        }
+
+        Map<String, String> stringStringMap = dokumanListesi.get(position);
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(stringStringMap.get("DokumanNumarasi"));
+        stringBuilder.append("-");
+        stringBuilder.append(stringStringMap.get("DokumanTipi"));
+        stringBuilder.append("-");
+        stringBuilder.append(stringStringMap.get("DokumanSevkTarihi"));
+        stringBuilder.append("-");
+        stringBuilder.append(stringStringMap.get("DokumanKayitEden"));
+
+        ((TextView) convertView.findViewById(R.id.dokuman_id)).setText(stringBuilder.toString());
+
+        return convertView;
+    }
+}
